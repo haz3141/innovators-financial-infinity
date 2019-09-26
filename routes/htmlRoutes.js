cb7577bc7eda8cf244bc3c5b493e3e83e6ed3473
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-26 13:11:17 -0400

    signup page basic up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,29 @@
 var db = require("../models");
+var authController = require("../controllers/authcontroller.js");
+
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg      : "Welcome!",
+        examples : dbExamples
       });
     });
   });
+
+  app.get("/landing", function(req, res) {
+    res.render("landing");
+  });
+
+  app.get("/signup", authController.signup);
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
-        example: dbExample
+        example : dbExample
       });
     });
   });
--- server.js
+++ server.js
@@ -7,16 +7,25 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
   "handlebars",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout : "main"
   })
 );
 app.set("view engine", "handlebars");
@@ -24,6 +33,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 var syncOptions = { force: false };
 
@@ -36,11 +46,7 @@ if (process.env.NODE_ENV === "test") {
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
 
