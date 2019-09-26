47a3c7482b8dff78ad702f3f54a0abc86f50f011
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-26 13:55:33 -0400

    finished signin auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-var authController = require("../controllers/authController.js");
+var authController = require("../controllers/authcontroller.js");
 
 module.exports = function(app, passport) {
   app.get("/signup", authController.signup);
@@ -17,6 +17,15 @@ module.exports = function(app, passport) {
   app.get("/dashboard", isLoggedIn, authController.dashboard);
 
   app.get("/logout", authController.logout);
+
+  app.post(
+    "/signin",
+    passport.authenticate("local-signin", {
+      successRedirect : "/dashboard",
+
+      failureRedirect : "/signin"
+    })
+  );
 
   function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
