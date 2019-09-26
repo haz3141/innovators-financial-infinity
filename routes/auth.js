8d4799933189f77a7454c1c3ab8403dd0e1bc17a
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-26 15:25:23 -0400

    more heroku

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-var authController = require("../controllers/authController.js");
+const authController = require("../controllers/authController.js");
 
 module.exports = function(app, passport) {
   app.get("/signup", authController.signup);
