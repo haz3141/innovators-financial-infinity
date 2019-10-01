2350ca7e79b79da083d3d16b9faf50d68c935ad7
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-30 20:21:57 -0400

    timed trading

--- public/js/trade.js
+++ public/js/trade.js
@@ -1,99 +1,56 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+// let time = Date.now();
+// console.log("time",time);
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+let price;
+let time;
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+function handlePut(event) {
   event.preventDefault();
+  let purchase = `
+  <li>
+      <p>Time: ${time}</p>
+      <p>Price: ${price}</p>
+      <p>Type: Put</p>
+    </li>`;
+  $("#purchase-list").append(purchase);
+}
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
+function handleCall(event) {
+  event.preventDefault();
+  let purchase = `
+  <li>
+      <p>Time: ${time}</p>
+      <p>Price: ${price}</p>
+      <p>Type: Call</p>
+    </li>`;
+  $("#purchase-list").append(purchase);
+}
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+function updateClock() {
+  // var now = new Date(), // current date
+  //     months = ['January', 'February', '...']; // you get the idea
+  //     time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+  //     // a cleaner way than string concatenation
+  //     date = [now.getDate(), 
+  //             months[now.getMonth()],
+  //             now.getFullYear()].join(' ');
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+  // // set the content of the element with the ID time to the formatted string
+  // document.getElementById('time').innerHTML = [date, time].join(' / ');
+  price = getRndInteger(1, 3);
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+  document.getElementById("price").innerHTML = price;
+  time = Date.now();
+  document.getElementById('time').innerHTML = time;
+  // call this function again in 1000ms
+  setInterval(updateClock, 1000);
+}
+updateClock();
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$("#option-put").on("click", handlePut);
+$("#option-call").on("click", handleCall);
